2e87b003639fed009bec9b0f85de73a0af22d4ab
Cameron Jenner <cjenner37@gmail.com>
2017-06-16 15:18:26 -0600

    letter boxes appear

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
+	var word, wordLength;
+	loadWord();
+
 	$('.btn-success').click(function () {
 			spinWheel();
 	})
 	
+	$('.alphabet').click(function () {
+		$letterChosen = $(this).data("letter");
+		$(this).css({'background-color': 'lightgrey', 'opacity': '.7', 'color': 'grey', 'border': '1px solid grey'});
+	})
 	
 })
 
+	function loadWord() {
+		word = "chameleon";
+		wordLength = word.length;
+		for (var i = 0; i < wordLength; i++) {
+			$('.letter-boxes').append(`<div class="col"></div>`)
+		}
+	}
 
 	function spinWheel() {
 		var prizes = [550, 800, 'jackpot', 'bankrupt', 600, 300, 3500, 600, 300, 700, 450, 350, 800, 'lose a turn', 300, 400, 600, 'bankrupt', 900, 'free spin', 500, 900, 300, 400];
@@ -14,4 +28,8 @@ $(document).ready(function () {
 		let degree_value = (multiplier * 15);
 		$wheel.css({'transform': 'rotate(' + degree_value + 'deg)'});
 		console.log(prizes[multiplier]);
+	}
+
+	function checkLetter($letterChosen) {
+		word.split("")
 	}
