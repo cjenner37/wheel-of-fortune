2a947240ea53f6ace37548325c18778cc8b3a6d3
Cameron Jenner <cjenner37@gmail.com>
2017-06-19 19:36:52 -0600

    bug fixes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var word, wordLength;
+var word, wordLength, $self;
 var alphabetDisabled = true;
 var consonantDisabled = false;
 var usedLetters = [];
+
 $(document).ready(function () {
 	loadWord();
 
@@ -32,7 +33,7 @@ $(document).ready(function () {
 				alert("You've already tried that one.")
 
 			} else if ($(this).hasClass('vowel')) {
-				let $self = $(this);
+				$self = $(this);
 				$('#myModal').modal();
 				$('#buy-vowel').click(function () {
 					$('#myModal').modal('toggle');
@@ -69,6 +70,8 @@ function loadWord() {
 	turnsLeft = 5;
 	$('#round span').text(round);
 	$('.letter-boxes').empty();
+	$('.alphabet').css({'background-color': '', 'opacity': '', 'color': 'black', 'border': '1px solid black'});
+	$('.vowel').css({'color': 'red',});
 	for (var i = 0; i < wordLength; i++) {
 		$('.letter-boxes').append(`<div class="col guess-boxes"></div>`)
 	}
@@ -83,6 +86,7 @@ function spinWheel() {
 	if (!Number.isInteger(prizes[multiplier])) {
 		if (prizes[multiplier] === 'bankrupt') {
 			bankValue = 0;
+			$('#bank span').text(bankValue);
 		} else if (prizes[multiplier] === 'lose a turn'){
 			turnsLeft--;
 		} else if (prizes[multiplier] === 'free spin'){
@@ -116,8 +120,8 @@ function buyVowel(letter) {
 				$('.guess-boxes')[i].append(letter);
 			}
 		}
-		$($self).css({'background-color': 'lightgrey', 'opacity': '.7', 'color': 'grey', 'border': '1px solid grey'});
-		$($self).addClass('used');
+		$self.css({'background-color': 'lightgrey', 'opacity': '.7', 'color': 'grey', 'border': '1px solid grey'});
+		$self.addClass('used');
 	} else {
 		alert("You don't have enough money!");
 	}
